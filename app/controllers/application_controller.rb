c260f7d25ae423fa670425f79f18eb097bc9f6f0
gogojojo <joseph.gochua@gmail.com>
2015-01-30 16:07:05 -0800

    added some stuff to display samples to my sample view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def index
+  	@player = Player.all
+  end
   def runit
   	TwitterJob.new.async.perform
   end
