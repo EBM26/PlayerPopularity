21199abb9201a288418dba8333dd34d95441a0ce
davidpetri <petridw@gmail.com>
2015-02-02 20:29:12 -0800

    moved methods for obtaining score totals for a player from hourly_score to the player model, where they should be.

--- app/models/hourly_score.rb
+++ app/models/hourly_score.rb
@@ -1,15 +1,2 @@
 class HourlyScore < ActiveRecord::Base
-  
-  #return array of hourly scores for this player from today
-  def today
-  end
-
-  #return array of hourly scores for this player from this week
-  def this_week
-  end
-
-  #return array of hourly scores for this player from this month
-  def this_month
-  end
-
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,27 @@
 class Player < ActiveRecord::Base
   has_many :hourly_scores
 
+  #return an array of all player's twitter handles
   def self.twitter_handles
     player_array = []
+
     Player.all.each do |player|
       player_array << player.twitter_handle
     end
+
     return player_array
   end
+
+  #return array of hourly scores for this player from today
+  def today
+  end
+
+  #return array of hourly scores for this player from this week, one per day
+  def this_week
+  end
+
+  #return array of hourly scores for this player from this month, one per day
+  def this_month
+  end
+
 end
