cc3496f21337b5c1bfc0d68abd3797276e3788df
davidpetri <petridw@gmail.com>
2015-02-02 20:25:55 -0800

    Add skeleton methods for hourly_score

--- app/models/hourly_score.rb
+++ app/models/hourly_score.rb
@@ -1,3 +1,15 @@
 class HourlyScore < ActiveRecord::Base
- 
+  
+  #return array of hourly scores for this player from today
+  def today
+  end
+
+  #return array of hourly scores for this player from this week
+  def this_week
+  end
+
+  #return array of hourly scores for this player from this month
+  def this_month
+  end
+
 end
