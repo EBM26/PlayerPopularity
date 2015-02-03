7151fe465a39904073532e53aa32906608a8998f
davidpetri <petridw@gmail.com>
2015-02-02 19:03:09 -0800

    modified api routes to make them more specific and remove unused routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
  
   root 'application#index'
 
-  get '/runit' => "application#runit"
+  get '/runit' => 'application#runit'
 
   resources :application
 
   namespace :api do
-    resources :players
-    resources :total_mentions
+    get '/players' => 'players#index'
+    get '/players/:id' => 'players#show'
+
+    get '/total_mentions' => 'total_mentions#index'
+    get '/total_mentions/:hour' => 'total_mentions#hour'
   end
 
 end
