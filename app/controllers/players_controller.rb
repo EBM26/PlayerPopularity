1b772a7cd45f675a93e43dbc64620a8e2a9be602
gogojojo <joseph.gochua@gmail.com>
2015-02-06 14:22:43 -0800

    adding more to the leaderboard

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
   
 
   def index
-    @players = Player.all.order(current_mentions: :desc)
+    @player = Player.all.order(current_mentions: :asc)
     # to set up a blank search bar
     @people = 'hi'
   end
