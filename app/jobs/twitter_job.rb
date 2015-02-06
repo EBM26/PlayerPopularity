4534be0d722649ff5dd7fd8e79c6627e356c5c64
gogojojo <joseph.gochua@gmail.com>
2015-02-05 19:16:43 -0800

    add the hourly scores are added at the beginning of the hour all players get a score at the same time regardless of whether the recieved any tweets or not

--- app/jobs/twitter_job.rb
+++ app/jobs/twitter_job.rb
@@ -14,72 +14,72 @@ class TwitterJob
 		end
 		# @topics = ['kobebryant','KingJames']
 		@client.filter(track: @topics.join(",")) do |object| 
-    	   if object.is_a?(Twitter::Tweet)
-          # calculating time for 24 hours
-
-                @hour = Time.now.hour
-                @day = Time.now.yday
-                
-                @topics.each do |thandle|
-                        if object.text.include? thandle 
-                          # checking if Total Mention is older than an hour
-                            if TotalMention.last.created_at.hour == @hour
-                                 TotalMention.last.update(total_mentions: (TotalMention.last.total_mentions + 1))
-                            else 
-                              #method to delete old ass mentions 
-                              TotalMention.all.each do |mention| 
-                                if (mention.created_at.yday != @day) && (mention.hour <= @hour)
-                                  mention.delete
-                                end
-                              end
-                              #method to create missing hours 
-                              @i = 0
-                              while @i < @hour do 
-                                if TotalMention.find_by(hour: @i)
-                                  puts 'already created'
-                                else
-                                 TotalMention.create(hour:@i, total_mentions: 1)
-                                end
-                                @i += 1
-                              end
-                            end
-                            # finding a particular player
-                             @a = Player.find_by(twitter_handle: thandle)
-                            # checking if player's scores are older than an hour
-                          if @a.updated_at.hour == @hour
-                               @a.current_mentions = (@a.current_mentions + 1)
-                               @a.save
-                          else 
-                            #method to delete old ass player scores
-                              @a.hourly_scores.all.each do |score| 
-                                if (score.created_at.yday != @day) && (score.hour <= @hour)
-                                  score.delete
-                                end
-                              end
-                            # This stuff is a little screwed up, ill fix it tomorrow
-                            @b = 0
-                            while @b < @hour  do 
-                                if @a.hourly_scores.find_by(hour: @b)
-                                  puts 'already created'
-                                else
-                                  if @b == @hour - 1
-                                    @total = TotalMention.find_by(hour:@b)
-                                    @a.hourly_scores.create(hour:@b, score:((@a.current_mentions.to_f/@total.total_mentions.to_f)*1000).round(2))
-                                    @a.current_mentions = 1
-                                    @a.save
-                                    puts 'has is a score'
-                                  else 
-                                    @a.hourly_scores.create(hour:@b, score: 0)
-                                  end
-                                end
-                                @b += 1
-                            end
-                          end
-                        end 	
-                end	 		 	
-		    end
+    	if object.is_a?(Twitter::Tweet)
+    # calculating time for 24 hours
+        @hour = Time.now.hour
+        @day = Time.now.yday        
+      # checking if Total Mention is older than an hour
+          if TotalMention.last.created_at.hour == @hour
+              TotalMention.last.update(total_mentions: (TotalMention.last.total_mentions + 1))
+          else 
+            #method to create missing hours 
+            @i = 0
+            while @i < @hour do 
+              if TotalMention.find_by(hour: @i)
+                  puts 'already created'
+              else
+                  TotalMention.create(hour:@i, total_mentions: 1)
+                end
+              @i += 1
+            end
+           #method to delete old ass mentions 
+              TotalMention.all.each do |mention| 
+                if (mention.created_at.yday != @day) && (mention.hour <= @hour)
+                    mention.delete
+                end
+                # creating all of the player scores at the same time
+                Player.all.each do |p|
+                #method to delete old ass player scores
+                  p.hourly_scores.all.each do |score| 
+                    if (score.created_at.yday != @day) && (score.hour <= @hour)
+                        score.delete
+                    end
+                end
+               #method to create the score 
+                @b = 0
+                while @b < @hour  do 
+                  if p.hourly_scores.find_by(hour: @b)
+                    puts 'already created'
+                  else
+                    if @b == @hour - 1
+                      @total = TotalMention.find_by(hour:@b)
+                      p.hourly_scores.create(hour:@b, score:((p.current_mentions.to_f/@total.total_mentions.to_f)*1000).round(2))
+                      p.current_mentions = 0
+                      p.save
+                      puts 'has is a score'
+                    else 
+                      p.hourly_scores.create(hour:@b, score: 0)
+                    end
+                  end
+                  @b += 1
+                end
+              end                
+      end
+    end
+    @topics.each do |thandle|
+      if object.text.include? thandle 
+      # finding a particular player
+        @a = Player.find_by(twitter_handle: thandle)
+      # checking if player's scores are older than an hour
+        if @a.updated_at.hour == @hour
+            @a.current_mentions = (@a.current_mentions + 1)
+            @a.save
         end
+      end 	
+    end	 		 	
 	end
+  end
+end
 
 	
 
