2e5d6bc7809b316813b982520a418275e4a796fc
gogojojo <joseph.gochua@gmail.com>
2015-02-10 12:51:11 -0800

    added a whole lot of css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202233351) do
+ActiveRecord::Schema.define(version: 20150210191813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150202233351) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.boolean  "switch"
+    t.string   "bggif"
   end
 
   create_table "total_mentions", force: :cascade do |t|
