326f597bfc3553e4ae6d593cbdae21cf1347ebf2
davidpetri <petridw@gmail.com>
2015-02-12 17:50:30 -0800

    added more tests for players api, hourly_score, and total_mention

--- spec/models/total_mention_spec.rb
+++ spec/models/total_mention_spec.rb
@@ -11,28 +11,29 @@ RSpec.describe TotalMention, :type => :model do
 
   describe "#create_missing_hours" do
 
-    # Needs fixing
-    # it "creates 24 empty total_mentions if there are no total_mentions" do
-    #   TotalMention.create_missing_hours
+    it "creates an empty total_mention for each hour between 12AM and the current hour if they're missing" do
+      TotalMention.create_missing_hours
 
-    #   expect(TotalMention.count).to eq(24)
-    # end
+      expected_count = Time.now.hour + 1
 
-    # Needs fixing
-    # it "creates only the missing total_mentions if some already exist" do
-    #   create(:total_mention, hour: 5, total_mentions: 50)
-    #   create(:total_mention, hour: 20, total_mentions: 80)
+      expect(TotalMention.count).to eq(expected_count)
+    end
 
-    #   TotalMention.create_missing_hours
+    it "creates only the missing total_mentions if some already exist" do
 
-    #   expect(TotalMention.count).to eq(24)
+      current_hour = Time.now.hour
+      expected_count = current_hour + 1
 
-    #   hour5 = TotalMention.find_by(hour: 5)
-    #   hour20 = TotalMention.find_by(hour: 20)
+      create(:total_mention, hour: current_hour, total_mentions: 50)
 
-    #   expect(hour5.total_mentions).to eq(50)
-    #   expect(hour20.total_mentions).to eq(80)
-    # end
+      TotalMention.create_missing_hours
+
+      expect(TotalMention.count).to eq(expected_count)
+
+      check_hour = TotalMention.find_by(hour: current_hour)
+
+      expect(check_hour.total_mentions).to eq(50)
+    end
 
   end
 
@@ -41,9 +42,6 @@ RSpec.describe TotalMention, :type => :model do
     it "does nothing if the total mentions are all from the last 24 hours" do
       mention = create(:total_mention, hour: hour+1, created_at: 1.day.ago)
       mention1 = create(:total_mention, hour: hour, created_at: Time.now)
-
-      # puts "Now Day: #{day}, hour: #{hour}"
-      # puts "Day: #{mention.created_at.yday}, Hour: #{mention.hour}"
 
       TotalMention.delete_old_hours
 
